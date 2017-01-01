955a24cefa8d19cae528f402c0f8ee98fb3efdd3
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-01-01 19:20:41 +0100

    Added collections to context

--- OGL/Models/OglContext.cs
+++ OGL/Models/OglContext.cs
@@ -3,11 +3,17 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace OGL.Models
 {
   public class OglContext : IdentityDbContext
   {
+    public DbSet<Category> Category { get; set; }
+    public DbSet<Advertisement> Advertisement { get; set; }
+    public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    public DbSet<Advertisement_Category> Advertisement_Category { get; set; }
+
     public OglContext() : base("DefaultConnection")
     {
     }
@@ -15,6 +21,22 @@ namespace OGL.Models
     public static OglContext Create()
     {
       return new OglContext();
+    }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+
+      // disable auto tables rename when generating database from code model
+      modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+      // disable default cascade delete and configure fluent api
+      modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+      modelBuilder.Entity<Advertisement>()
+        .HasRequired(x => x.ApplicationUser)
+        .WithMany(x => x.Advertisement)
+        .HasForeignKey(x => x.ApplicationUserId)
+        .WillCascadeOnDelete(true);
     }
   }
 }
