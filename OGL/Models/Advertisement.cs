938b64a63155974b9e3b4ef967ac4e74ca05ca6f
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-01-01 17:47:10 +0100

    Added Advertisement model

---
+++ OGL/Models/Advertisement.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OGL.Models
+{
+  public class Advertisement
+  {
+    [Key]
+    [Display(Name = "Id")]
+    public int Id { get; set; }
+
+    [Display(Name = "Advertisement content")]
+    [MaxLength(500)]
+    [Required]
+    public string Content { get; set; }
+
+    [Display(Name = "Advertisement title")]
+    [MaxLength(72)]
+    [Required]
+    public string Title { get; set; }
+
+    [Display(Name = "Add date")]
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+    [Required]
+    public DateTime AddDateTime { get; set; }
+
+    [Required]
+    public string ApplicationUserId { get; set; }
+
+    public virtual ICollection<Advertisement_Category> Advertisement_Category { get; set; }
+
+    public virtual ApplicationUser ApplicationUser { get; set; }
+
+    public Advertisement()
+    {
+      Advertisement_Category = new HashSet<Advertisement_Category>();
+    }
+  }
+}
