d093cc8a88cbe50b8e6fa6a43f17ad806eafd938
Arkadiusz Gabrys <arkadiuszgabrys@gmail.com>
2017-01-01 18:49:14 +0100

    Changed ApplicationDbContext to OglContext

---
+++ OGL/Models/OglContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace OGL.Models
+{
+  public class OglContext : IdentityDbContext
+  {
+    public OglContext() : base("DefaultConnection")
+    {
+    }
+
+    public static OglContext Create()
+    {
+      return new OglContext();
+    }
+  }
+}
